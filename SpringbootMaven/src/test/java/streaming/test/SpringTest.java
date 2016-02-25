33697279bb583afb085026eceafea473d2cff456
quicotte-formation <quicotte@gmail.com>
2016-02-25 20:49:50 +0100

    Configuration OK.

---
+++ SpringbootMaven/src/test/java/streaming/test/SpringTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import streaming.spring.SpringConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * @author ETY
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes=SpringConfig.class)
+public class SpringTest {
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Test
+    public void doNadaOK(){
+        
+    }
+    
+}
