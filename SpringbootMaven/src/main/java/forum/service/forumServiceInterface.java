384ba3b79c44c92e53e3dd00fb582c227de07f52
vfiatte <valentin.fiatte@gmail.com>
2016-02-26 12:22:53 +0100

    Interface Service créées

---
+++ SpringbootMaven/src/main/java/forum/service/forumServiceInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forum.service;
+
+import forum.entity.Forum;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author admin
+ */
+public interface forumServiceInterface extends CrudRepository<Forum, Long> {
+    
+}
